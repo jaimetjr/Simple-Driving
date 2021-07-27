484ddaac1a8bd468084c6f942f30d82084fecbc4
jaimetjr <jaime.tasca.jr@gmail.com>
2021-07-26 23:22:00 -0300

    add collision boxes

--- Simple Driving/Assets/Scripts/Car.cs
+++ Simple Driving/Assets/Scripts/Car.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Car : MonoBehaviour
 {
@@ -16,6 +17,14 @@ public class Car : MonoBehaviour
 
         transform.Rotate(0f, steerValue * _turnSpeed * Time.deltaTime, 0f);
         transform.Translate(Vector3.forward * _speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Obstacle"))
+        {
+            SceneManager.LoadScene("Scene_MainMenu");
+        }
     }
 
     public void Steer(int value)
